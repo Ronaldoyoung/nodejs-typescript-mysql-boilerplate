3d31875c116be8db5723fa061e225f18c15517dd
윤 두영 <ronaldoyoung@gmail.com>
2021-11-18 10:16:15 +0900

    src 폴더 삭제 후 root 경로로 이동

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
-import Logger from './loaders/logger';
+import Logger from '@/loaders/logger';
 import config from '@/config';
 
 async function startServer() {
   const app: express.Application = express();
 
-  app.listen(8000, () => {
+  app.listen(config.port, () => {
     Logger.info(`
     ################################################
     🛡️  Server listening on port: ${config.port} 🛡️
